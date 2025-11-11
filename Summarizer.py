3efd96f2dbf8d7803a0bc814c3d7e3a863742090
Tobias <tobias.schmelzle@gmx.de>
2025-11-11 17:36:30 +0100

    changed to youtube-transcript-api

--- Summarizer.py
+++ Summarizer.py
@@ -1,16 +1,16 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.document_loaders import YoutubeLoader
+from youtube_transcript_api import YouTubeTranscriptApi
 from langchain_core.prompts import PromptTemplate
 
 def summarize(url, scope, language):
     llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash") 
-    llm.google_api_key = "YOUR_GOOGLE_API_KEY"  # Replace with your actual API key
+    llm.google_api_key = "YOUR_API_KEY"  # Replace with your actual API key
 
-    loader = YoutubeLoader.from_youtube_url(url, add_video_info=False, language=["en", "de", "fr", "it", "es"])
-    transcript = loader.load()[0].page_content
+    transcript = YouTubeTranscriptApi.get_transcript(url, languages=["en", "de", "fr", "it", "es"])
+    text = " ".join([t['text'] for t in transcript])
 
     promptTemplate = """
-    You summarize the content of Youtube videos. For that you are given the following transcript {transcript}. 
+    You summarize the content of Youtube videos. For that you are given the following transcript: {transcript}. 
     The summary should be {scope}.
     The summary should be in the language {language}.
     Don't include any other information. Don't mention that you are referencing a video in the summary. Use fitting headlines and paragraphs.
@@ -20,5 +20,5 @@ def summarize(url, scope, language):
     
     # Create the chain and stream the output
     chain = prompt | llm
-    return chain.invoke({"transcript": transcript, "scope": scope, "language": language}).content
+    return chain.invoke({"transcript": text, "scope": scope, "language": language}).content
 
